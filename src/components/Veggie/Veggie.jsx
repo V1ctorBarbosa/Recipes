acf6a7834a6004b961172c3bf4fb632f152a27ad
Victor Barbosa <vicfer2708@gmail.com>
2022-07-13 16:13:40 -0300

    recipes

--- src/components/Veggie/Veggie.jsx
+++ src/components/Veggie/Veggie.jsx
@@ -20,18 +20,12 @@ function Veggie() {
 
   const getVeggie = async () => {
 
-    const check = localStorage.getItem('veggie');
-
-    if (check) {
-      setVeggie(JSON.parse(check))
-    } else {
       const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&tags=vegetarian&number=10`);
       const data = await api.json()
 
       localStorage.setItem('veggie', JSON.stringify(data.recipes))
       setVeggie(data.recipes)
       console.log(data.recipes)
-    }
   }
 
   return <div>
